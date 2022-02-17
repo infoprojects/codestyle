e84fdf9bb1dec8f0675ce342a3564e3059e74d12
Thomas Rijsewijk <thomas.rijsewijk@infoprojects.nl>
2022-02-17 10:53:42 +0100

    Initial ruleset

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
 module.exports = {
+  extends: 'stylelint-config-recommended-scss',
+  reportNeedlessDisables: true,
+  reportInvalidScopeDisables: true,
+  reportDescriptionlessDisables: true,
+  defaultSeverity: 'error',
+  rules: {
+    'number-leading-zero': 'always'
+  }
 };
