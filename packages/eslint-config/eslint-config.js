2dc7b8ceef880e479461008d7ef1bfc73352aa0b
Thomas Rijsewijk <thomas.rijsewijk@infoprojects.nl>
2022-11-24 10:44:40 +0100

    eslint-config: no-console en object-shorthand

--- packages/eslint-config/eslint-config.js
+++ packages/eslint-config/eslint-config.js
@@ -3,7 +3,14 @@ module.exports = {
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   rules: {
     eqeqeq: ["error"],
+    "no-console": [
+      "error",
+      {
+        allow: ["assert", "debug", "error", "group", "info", "table", "trace", "warn"],
+      },
+    ],
     "no-else-return": ["error", { allowElseIf: false }],
+    "object-shorthand": ["error"],
     "@typescript-eslint/no-empty-interface": ["off"],
     "@typescript-eslint/no-unused-vars": [
       "error",
